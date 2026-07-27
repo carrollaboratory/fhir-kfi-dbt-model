7839e232d175720884e81762678d9d8af98ebc94
Eric Torstenson <eric.s.torstenson@vumc.org>
2026-07-27 18:06:36 -0500

    Fixed provision and macro
    A couple of issues were interfering with fixing the issue with provisions, including the fact that the new script won't run until this script's output is valid. Gott love chicken and egg coding problems.

--- fhir_kfi_dbt_model/macros/combined_codings.sql
+++ fhir_kfi_dbt_model/macros/combined_codings.sql
@@ -1,16 +1,17 @@
 -- macros/render_combined_coding.sql
 {% macro render_combined_coding(stg_alias, col1, col2, col3) %}
-    (
-        SELECT COALESCE(
-            jsonb_agg(map.fhir_coding),
-            -- Fallback empty or unknown coding array if none of the columns matched
-            jsonb_build_array(jsonb_build_object(
-                'system', 'http://example.org',
-                'code', 'unknown',
-                'display', 'No Mappings Found'
-            ))
-        )
-        FROM {{ ref('concept_mappings') }} AS map
-        WHERE map.local_code IN ({{ stg_alias }}.{{ col1 }}, {{ stg_alias }}.{{ col2 }}, {{ stg_alias }}.{{ col3 }})
-    )
+(
+  SELECT COALESCE(
+    (SELECT jsonb_agg(fhir_coding) FROM {{ ref('concept_mappings') }} WHERE local_code = {{ stg_alias }}.{{ col1 }}),
+    jsonb_build_array()
+  ) ||
+  COALESCE(
+    (SELECT jsonb_agg(fhir_coding) FROM {{ ref('concept_mappings') }} WHERE local_code = {{ stg_alias }}.{{ col2 }}),
+    jsonb_build_array()
+  ) ||
+  COALESCE(
+    (SELECT jsonb_agg(fhir_coding) FROM {{ ref('concept_mappings') }} WHERE local_code = {{ stg_alias }}.{{ col3 }}),
+    jsonb_build_array()
+  )
+)
 {% endmacro %}
