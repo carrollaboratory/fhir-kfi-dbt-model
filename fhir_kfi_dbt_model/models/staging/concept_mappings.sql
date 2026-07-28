19e295f8f525be787649b9636fda002c888ebc5e
Eric Torstenson <eric.s.torstenson@vumc.org>
2026-07-28 09:25:50 -0500

    Adding in some leftover files that should be tracked that were overlooked during cleanup sweep of my working directory

---
+++ fhir_kfi_dbt_model/models/staging/concept_mappings.sql
@@ -0,0 +1,12 @@
+
+{{ config(materialized='ephemeral') }}
+
+SELECT
+    local_code,
+    -- This converts the columns directly into the FHIR coding JSON structure
+    jsonb_build_object(
+        'system', code_system,
+        'code', code,
+        'display', display
+    ) AS fhir_coding
+FROM {{ ref('harmony') }}
